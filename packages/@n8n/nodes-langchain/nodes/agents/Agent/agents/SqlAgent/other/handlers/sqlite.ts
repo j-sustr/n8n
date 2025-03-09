ceed3d8f8489da2385e4c57dc18cbda13d14e978
Jan Šustr <jan.sustr11@gmail.com>
2025-03-09 19:48:00 +0100

    disable sqlite

--- packages/@n8n/nodes-langchain/nodes/agents/Agent/agents/SqlAgent/other/handlers/sqlite.ts
+++ packages/@n8n/nodes-langchain/nodes/agents/Agent/agents/SqlAgent/other/handlers/sqlite.ts
@@ -2,7 +2,7 @@ import { DataSource } from '@n8n/typeorm';
 import * as fs from 'fs';
 import type { IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
 import { BINARY_ENCODING, NodeOperationError } from 'n8n-workflow';
-import * as sqlite3 from 'sqlite3';
+// import * as sqlite3 from 'sqlite3';
 import * as temp from 'temp';
 
 export async function getSqliteDataSource(
@@ -35,12 +35,12 @@ export async function getSqliteDataSource(
 	fs.writeFileSync(tempDbPath, bufferString);
 
 	// Initialize a new SQLite database from the temp file
-	const tempDb = new sqlite3.Database(tempDbPath, (error: Error | null) => {
-		if (error) {
-			throw new NodeOperationError(this.getNode(), 'Could not connect to database');
-		}
-	});
-	tempDb.close();
+	// const tempDb = new sqlite3.Database(tempDbPath, (error: Error | null) => {
+	// 	if (error) {
+	// 		throw new NodeOperationError(this.getNode(), 'Could not connect to database');
+	// 	}
+	// });
+	// tempDb.close();
 
 	return new DataSource({
 		type: 'sqlite',
